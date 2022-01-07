3032e7b3baabbc798ac34b3a9444e49df8ba1b90
Vadim Oleynik <gauchoyezweek@gmail.com>
2022-01-08 00:39:27 +0300

    Добавил несколько тестов

--- src/test/utils.test.tsx
+++ src/test/utils.test.tsx
@@ -1,25 +1,13 @@
 import { filterListById } from '../utils/utils';
 
 it('foo filter list is work', () => {
-  expect(
-    filterListById(
-      [
-        { id: '1', isActive: false },
-        { id: '2', isActive: true },
-      ],
-      '2',
-    ),
-  ).toEqual([{ id: '1', isActive: false }]);
+  expect(filterListById([{ id: '2', isActive: false }], '1')).toEqual([{ id: '2', isActive: false }]);
 });
 
 it('foo filter list get warning', () => {
-  expect(
-    filterListById(
-      [
-        { id: '12', isActive: false },
-        { id: '13', isActive: true },
-      ],
-      '2',
-    ),
-  ).toEqual([{ id: '1', isActive: false }]);
+  expect(filterListById([{ id: '12', isActive: false }], '2')).toEqual([{ id: '12', isActive: false }]);
+});
+
+it('foo filter list get warning', () => {
+  expect(filterListById([{ id: '14', isActive: false }], '3')).toEqual([{ id: '14', isActive: false }]);
 });
